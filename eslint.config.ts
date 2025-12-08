01ffd195036569cddf667f21f5d06915a88e832c
Baheya <khalifa.baheya@gmail.com>
2025-12-08 12:58:52 -0500

    uncommented parser options so they can take effect

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,17 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 export default [
-    // add more generic rule sets here, such as:
-    // js.configs.recommended,
-    ...eslintPluginAstro.configs.recommended,
-    ...eslintPluginAstro.configs["jsx-a11y-recommended"],
-    {
-        files: ["*.astro"],
-        parser: "astro-eslint-parser",
-        // parserOptions: {
-        //   parser: "@typescript-eslint/parser",
-        //   extraFileExtensions: [".astro"],
-        //   project: "./tsconfig.eslint.json",
-        // },
-        // ...
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
+  {
+    files: ["*.astro"],
+    parser: "astro-eslint-parser",
+    parserOptions: {
+      parser: "@typescript-eslint/parser",
+      extraFileExtensions: [".astro"],
+      project: "./tsconfig.eslint.json",
     },
+    // ...
+  },
 ];
