729fea1da19677b22e1cf0c780f92aec6be8c689
Baheya <khalifa.baheya@gmail.com>
2025-12-08 13:41:52 -0500

    updated config again
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again
    
    updated config again

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,28 +1,34 @@
 import eslintPluginAstro from "eslint-plugin-astro";
-export default [
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default defineConfig([
   // add more generic rule sets here, such as:
   // js.configs.recommended,
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   {
+    files: ["**/*.astro,**/*.ts,**/*.tsx"],
+    ignores: ["./.astro/**/*"],
+    processor: "astro/client-side-ts",
     languageOptions: {
       parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: "./tsconfig.eslint.json",
-        extraFileExtensions: [".astro"],
+      globals: {
+        ...globals.browser,
+        ...globals.node,
       },
-    },
-  },
-  {
-    files: ["*.astro"],
-    languageOptions: {
-      parser: "astro-eslint-parser",
       parserOptions: {
-        parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
         project: "./tsconfig.eslint.json",
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
     // ...
   },
-];
+]);
