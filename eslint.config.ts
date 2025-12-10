da417c74e1d8d48fca640691fff5a32b4fbcb31d
Baheya <khalifa.baheya@gmail.com>
2025-12-10 13:09:22 -0500

    fixed config structure

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,30 +5,25 @@ import tseslint from "typescript-eslint";
 import globals from "globals";
 
 export default defineConfig([
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   {
-    files: ["**/*.astro,**/*.ts,**/*.tsx"],
-    ignores: ["./.astro/**/*"],
+    files: ["**/*.{astro,ts,tsx}"],
     processor: "astro/client-side-ts",
     languageOptions: {
-      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        projectService: "tsconfig.eslint.json",
+      },
       globals: {
         ...globals.browser,
         ...globals.node,
       },
-      parserOptions: {
-        extraFileExtensions: [".astro"],
-        project: "./tsconfig.eslint.json",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
     },
-    // ...
+  },
+  {
+    ignores: [".astro/**"],
   },
 ]);
