5aa68065998c47c6c8b081f155dd0bc8425b2750
Baheya <khalifa.baheya@gmail.com>
2026-02-17 11:00:05 -0500

    added module declaration to fix import errors in ts files

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,9 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare module "*.astro" {
+  import type { AstroComponentFactory } from "astro/runtime/server";
+  const component: AstroComponentFactory;
+  export default component;
+}
