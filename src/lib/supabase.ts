7444ca034933343d6f6bfff9763c4e744d08e16b
Baheya <khalifa.baheya@gmail.com>
2026-04-16 13:29:09 -0400

    commented out server clients because of a ts issue with supabase/ssr

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,54 +1,54 @@
 import type { Database } from "#db/types.ts";
-import type { APIContext } from "astro";
+// import type { APIContext } from "astro";
 
-import {
-  createServerClient,
-  parseCookieHeader,
-  serializeCookieHeader,
-  type CookieMethodsServer,
-  type CookieOptions,
-} from "@supabase/ssr";
+// import {
+//   type CookieMethodsServer,
+//   type CookieOptions,
+//   createServerClient,
+//   parseCookieHeader,
+//   serializeCookieHeader,
+// } from "@supabase/ssr";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseKey = import.meta.env.SUPABASE_KEY;
 
-export const createServerClientInstance = (context: APIContext) => {
-  const headers = new Headers();
+// export const createServerClientInstance = (context: APIContext) => {
+//   const headers = new Headers();
 
-  if (!import.meta.env.SUPABASE_URL || !import.meta.env.SUPABASE_KEY) {
-    throw new Error("Missing Supabase Environment Variables");
-  }
+//   if (!import.meta.env.SUPABASE_URL || !import.meta.env.SUPABASE_KEY) {
+//     throw new Error("Missing Supabase Environment Variables");
+//   }
 
-  const cookies: CookieMethodsServer = {
-    getAll() {
-      const cookieHeader = context.request.headers.get("Cookie");
-      if (!cookieHeader) {
-        return null;
-      }
-      return parseCookieHeader(cookieHeader).map(({ name, value }) => ({
-        name,
-        value: value ?? "",
-      }));
-    },
-    setAll(
-      cookiesToSet: {
-        name: string;
-        value: string;
-        options: CookieOptions;
-      }[],
-    ) {
-      cookiesToSet.forEach(({ name, value, options }) => {
-        headers.append(
-          "Set-Cookie",
-          serializeCookieHeader(name, value, options),
-        );
-      });
-    },
-  };
-  return createServerClient<Database, "public">(supabaseUrl, supabaseKey, {
-    cookies: cookies,
-  });
-};
+//   const cookies: CookieMethodsServer = {
+//     getAll() {
+//       const cookieHeader = context.request.headers.get("Cookie");
+//       if (!cookieHeader) {
+//         return null;
+//       }
+//       return parseCookieHeader(cookieHeader).map(({ name, value }) => ({
+//         name,
+//         value: value ?? "",
+//       }));
+//     },
+//     setAll(
+//       cookiesToSet: {
+//         name: string;
+//         value: string;
+//         options: CookieOptions;
+//       }[],
+//     ) {
+//       cookiesToSet.forEach(({ name, value, options }) => {
+//         headers.append(
+//           "Set-Cookie",
+//           serializeCookieHeader(name, value, options),
+//         );
+//       });
+//     },
+//   };
+//   return createServerClient<Database, "public">(supabaseUrl, supabaseKey, {
+//     cookies: cookies,
+//   });
+// };
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,65 +1,65 @@
-import type { SupabaseClient } from "@supabase/supabase-js";
-import type { Database } from "#db/types.ts";
+// import type { SupabaseClient } from "@supabase/supabase-js";
+// import type { Database } from "#db/types.ts";
 
-import {
-  type CookieMethodsServer,
-  parseCookieHeader,
-  type CookieOptions,
-  serializeCookieHeader,
-  createServerClient,
-} from "@supabase/ssr";
-import { defineMiddleware } from "astro:middleware";
+// import {
+//   type CookieMethodsServer,
+//   parseCookieHeader,
+//   type CookieOptions,
+//   serializeCookieHeader,
+//   createServerClient,
+// } from "@supabase/ssr";
+// import { defineMiddleware } from "astro:middleware";
 
-// `context` and `next` are automatically typed
-export const onRequest = defineMiddleware(async (context, next) => {
-  try {
-    const supabaseUrl = import.meta.env.SUPABASE_URL;
-    const supabaseKey = import.meta.env.SUPABASE_KEY;
-    const headers = new Headers();
+// // `context` and `next` are automatically typed
+// export const onRequest = defineMiddleware(async (context, next) => {
+//   try {
+//     const supabaseUrl = import.meta.env.SUPABASE_URL;
+//     const supabaseKey = import.meta.env.SUPABASE_KEY;
+//     const headers = new Headers();
 
-    if (!import.meta.env.SUPABASE_URL || !import.meta.env.SUPABASE_KEY) {
-      throw new Error("Missing Supabase Environment Variables");
-    }
+//     if (!import.meta.env.SUPABASE_URL || !import.meta.env.SUPABASE_KEY) {
+//       throw new Error("Missing Supabase Environment Variables");
+//     }
 
-    const cookies: CookieMethodsServer = {
-      getAll() {
-        const cookieHeader = context.request.headers.get("Cookie");
-        if (!cookieHeader) {
-          return null;
-        }
-        return parseCookieHeader(cookieHeader).map(({ name, value }) => ({
-          name,
-          value: value ?? "",
-        }));
-      },
-      setAll(
-        cookiesToSet: {
-          name: string;
-          value: string;
-          options: CookieOptions;
-        }[],
-      ) {
-        cookiesToSet.forEach(({ name, value, options }) => {
-          headers.append(
-            "Set-Cookie",
-            serializeCookieHeader(name, value, options),
-          );
-        });
-      },
-    };
+//     const cookies: CookieMethodsServer = {
+//       getAll() {
+//         const cookieHeader = context.request.headers.get("Cookie");
+//         if (!cookieHeader) {
+//           return null;
+//         }
+//         return parseCookieHeader(cookieHeader).map(({ name, value }) => ({
+//           name,
+//           value: value ?? "",
+//         }));
+//       },
+//       setAll(
+//         cookiesToSet: {
+//           name: string;
+//           value: string;
+//           options: CookieOptions;
+//         }[],
+//       ) {
+//         cookiesToSet.forEach(({ name, value, options }) => {
+//           headers.append(
+//             "Set-Cookie",
+//             serializeCookieHeader(name, value, options),
+//           );
+//         });
+//       },
+//     };
 
-    const supabase = createServerClient<Database, "public">(
-      supabaseUrl,
-      supabaseKey,
-      {
-        cookies: cookies,
-      },
-    ) as unknown as SupabaseClient;
+//     const supabase = createServerClient<Database, "public">(
+//       supabaseUrl,
+//       supabaseKey,
+//       {
+//         cookies: cookies,
+//       },
+//     ) as unknown as SupabaseClient;
 
-    context.locals.supabase = supabase;
-    context.locals.headers = headers;
-  } catch (e) {
-    console.log(e);
-  }
-  return next();
-});
+//     context.locals.supabase = supabase;
+//     context.locals.headers = headers;
+//   } catch (e) {
+//     console.log(e);
+//   }
+//   return next();
+// });
