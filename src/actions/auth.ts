a4a66019ea6656cad9c655fe1ec0125d5cc94651
Baheya <khalifa.baheya@gmail.com>
2026-04-27 11:44:35 -0400

    added sign in page

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,7 +1,7 @@
 import { defineAction } from "astro:actions";
 
 export const auth = {
-  signin: defineAction({
+  signup: defineAction({
     handler: async (_, context) => {
       const formData = await context.request.formData();
       const email = formData.get("email")?.toString();
@@ -31,6 +31,36 @@ export const auth = {
       }
 
       return new Response("Successfully signed in", { status: 200 });
+    },
+  }),
+  signin: defineAction({
+    handler: async (_, context) => {
+      const formData = await context.request.formData();
+      const email = formData.get("email")?.toString();
+      const password = formData.get("password")?.toString();
+
+      if (!email || !password) {
+        return new Response("Email and password are required", { status: 400 });
+      }
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const { data, error } =
+        await context.locals.supabase.auth.signInWithPassword({
+          email,
+          password,
+        });
+
+      if (error) {
+        return new Response(error.message, { status: 500 });
+      }
+
+      const { access_token, refresh_token } = data.session;
+      context.cookies.set("sb-access-token", access_token, {
+        path: "/",
+      });
+      context.cookies.set("sb-refresh-token", refresh_token, {
+        path: "/",
+      });
     },
   }),
 };
