0a05e16a39cc7e5f34ce225c9dbaf1ba37093ef0
Baheya <khalifa.baheya@gmail.com>
2026-05-01 08:27:58 -0400

    stringifying body in fetch wrapper

--- src/db/fetch.ts
+++ src/db/fetch.ts
@@ -8,12 +8,14 @@ export const fetchData = async <T>(
   // console.log(fetchUrl);
   const options: RequestInit = {
     method: method as string,
-    body: body as unknown as BodyInit | null,
+    body: JSON.stringify(body) as unknown as BodyInit | null,
   };
+
   const response = await fetch(path, options);
   if (!response.ok) {
     throw new Error(`Response status: ${response.status}`);
   }
+
   const data = await response.json();
   return data as T;
 };
