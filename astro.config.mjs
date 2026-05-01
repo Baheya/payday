dfee079b7b21beeee64f8f01fdbb08bedebff6f4
Baheya <khalifa.baheya@gmail.com>
2026-05-01 08:26:25 -0400

    enabled local https

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify";
+import { readFileSync } from "node:fs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +9,13 @@ export default defineConfig({
   adapter: netlify(),
   devToolbar: {
     enabled: false,
+  },
+  vite: {
+    server: {
+      https: {
+        cert: readFileSync("./cert.pem"),
+        key: readFileSync("./key.pem"),
+      },
+    },
   },
 });
